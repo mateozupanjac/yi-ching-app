48d34071682e625802ec879b0d292b1a3a3b6ea9
mateozupanjac <zupa93@windowslive.com>
2021-10-25 20:08:37 +0200

    Added and declared routes to <App> component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+import UserLoginPage from "./pages/UserLogin";
+import InstructionsPage from "./pages/Instructions";
+import YiChingPage from "./pages/YiChingPage";
+import UserSavedQuestionsPage from "./pages/UserSavedQuestions";
+import UserSettings from "./pages/UserSettings";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Git Init</h1>
-    </div>
+    <Switch>
+      <Route path="/login">
+        <UserLoginPage />
+      </Route>
+      <Route path="/instructions">
+        <InstructionsPage />
+      </Route>
+      <Route path="/yi-ching">
+        <YiChingPage />
+      </Route>
+      <Route path="user-saved-questions">
+        <UserSavedQuestionsPage />
+      </Route>
+      <Route path="user-settings">
+        <UserSettings />
+      </Route>
+      <Route path="user-profile">
+        <UserProfile />
+      </Route>
+    </Switch>
   );
 }
 
