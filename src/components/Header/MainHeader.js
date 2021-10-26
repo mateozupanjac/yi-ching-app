ba8f994b6a3deb125f82f8c2e8ae3c8742dd931e
mateozupanjac <zupa93@windowslive.com>
2021-10-26 22:47:19 +0200

    Created Navigation component and added a dropdown menu, add some css

--- src/components/Header/MainHeader.js
+++ src/components/Header/MainHeader.js
@@ -1,6 +1,5 @@
-import { Link, NavLink } from "react-router-dom";
-
 import classes from "./MainHeader.module.css";
+import Navigation from "../Navigation/Navigation";
 
 const MainHeader = () => {
   return (
@@ -9,31 +8,7 @@ const MainHeader = () => {
       <div className={classes.userProfileImg}>
         <img alt="user-img" src="/" />
       </div>
-      <nav>
-        <ul className={classes["list-items"]}>
-          <li className={classes["list-item"]}>
-            <NavLink activeClassName={classes.active} to="/yi-ching">
-              Yi Ching
-            </NavLink>
-          </li>
-          <li className={classes["list-item"]}>
-            <NavLink activeClassName={classes.active} to="instructions">
-              Instructions
-            </NavLink>
-          </li>
-          <li className={classes["list-item"]}>
-            <NavLink activeClassName={classes.active} to="user-saved-questions">
-              My Questions
-            </NavLink>
-          </li>
-          <li className={classes["list-item"]}>
-            <Link>More</Link>
-          </li>
-          <li className={classes["list-item"]}>
-            <Link to="/login">Logout</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navigation />
     </header>
   );
 };
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,38 @@
+import { Link, NavLink } from "react-router-dom";
+import classes from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul className={classes["list-items"]}>
+        <li className={classes["list-item"]}>
+          <NavLink activeClassName={classes.active} to="/yi-ching">
+            Yi Ching
+          </NavLink>
+        </li>
+        <li className={classes["list-item"]}>
+          <NavLink activeClassName={classes.active} to="/instructions">
+            Instructions
+          </NavLink>
+        </li>
+        <li className={classes["list-item"]}>
+          <NavLink activeClassName={classes.active} to="/user-saved-questions">
+            My Questions
+          </NavLink>
+        </li>
+        <div className={classes["dropdown"]}>
+          <div className={classes["dropdown-btn"]}>Menu</div>
+          <div className={classes["dropdown-content"]}>
+            <Link to="/user-profile">My Profile</Link>
+            <Link to="/user-settings">Settings</Link>
+          </div>
+        </div>
+        <li className={classes["list-item"]}>
+          <Link to="/login">Logout</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
